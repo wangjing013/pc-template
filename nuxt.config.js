e6bd2c823479c16263002ebcb1b447522d0b9322
wangjing <974742841@qq.com>
2022-08-26 10:53:05 +0800

    feat: add simplebar

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,12 @@ export default {
   css: ['ant-design-vue/dist/antd.css', '~/assets/styles/common.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/antd-ui', '@/plugins/axios', '@/plugins/error-catch'],
+  plugins: [
+    '@/plugins/antd-ui',
+    '@/plugins/axios',
+    '@/plugins/error-catch',
+    '@/plugins/simplebar.client',
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: ['~/components'],
---
+++ src/plugins/simplebar.client.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import simplebar from 'simplebar-vue'
+import 'simplebar/dist/simplebar.min.css'
+Vue.component(simplebar.name, simplebar)
